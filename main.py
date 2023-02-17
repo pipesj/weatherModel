df70defc40ab4faf065a2d45e4c9d461fd13506f
Merek Pipes' VM <pipesj@union.edu>
2023-02-17 01:08:37 -0500

    debugging

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pandas.io.json import json_normalize
 coxsackieURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Coxsackie%2C%20NY/next7days?unitGroup=metric&include=hours&key=9RGSNMG7YSS4ZZ8PMSVTGUGRX&contentType=json"
 r = requests.get(coxsackieURL)
 json = r.json()
+print("Got to json")
+print("pandas version: "+ print(pd.__version__))
 hourly_data = pd.json_normalize(json, record_path=['days', 'hours'])
 
 kept_columns = ["datetime","temp","feelslike","dew", "humidity", "precip", "precipprob","snow","snowdepth","windspeed","winddir","pressure","cloudcover","visibility","solarradiation","uvindex"]
