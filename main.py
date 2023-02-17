5c9c3e6a6bc237a34fa93a6e07cceda0206bc77b
Merek Pipes' VM <pipesj@union.edu>
2023-02-17 15:32:18 -0500

    demo

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import requests
 from pandas.io.json import json_normalize
-import prophet as fb
+## import prophet as fb
 
 coxsackieURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Coxsackie%2C%20NY/next7days?unitGroup=metric&include=hours&key=9RGSNMG7YSS4ZZ8PMSVTGUGRX&contentType=json"
 r = requests.get(coxsackieURL)
 json = r.json()
 hourly_data = json_normalize(json, record_path=['days', 'hours'])
-print(fb.__version__)
+## print(fb.__version__)
 
 kept_columns = ["datetime","temp","feelslike","dew", "humidity", "precip", "precipprob","snow","snowdepth","windspeed","winddir","pressure","cloudcover","visibility","solarradiation","uvindex"]
 hourly_data['datetime'] = pd.to_datetime(hourly_data['datetimeEpoch'], unit='s')
