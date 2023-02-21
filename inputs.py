481493405cd02fe97e3f0093b2666a0c175d7700
Merek Pipes' VM <pipesj@union.edu>
2023-02-21 11:00:56 -0500

    bme280

--- inputs.py
+++ inputs.py
@@ -1,3 +1,9 @@
 from periphery import I2C
 
 i2c1 = I2C("/dev/i2c-3")  # pins 3/5
+
+msgs = [I2C.Message([0x01, 0x00]), I2C.Message([0x00], read=True)]
+i2c1.transfer(0x50, msgs)
+print("0x100: 0x{:02x}".format(msgs[1].data[0]))
+
+i2c1.close()
