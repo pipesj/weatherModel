f08ce5d71af7279c07ef3c8a12c71be7980a04f4
Merek Pipes' VM <pipesj@union.edu>
2023-02-21 23:52:51 -0500

    bme280

--- inputs.py
+++ inputs.py
@@ -1,7 +1,8 @@
 from periphery import I2C
 import time
-import board
 from adafruit_bme280 import basic as adafruit_bme280
+import board
+
 print("Successful imports")
 i2c = board.I2C()
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
