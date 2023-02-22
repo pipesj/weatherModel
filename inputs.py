a7d3b1c9704f02a3b6cc4a3b91f1473d0615c0f2
Merek Pipes' VM <pipesj@union.edu>
2023-02-21 23:49:43 -0500

    bme280

--- inputs.py
+++ inputs.py
@@ -1,8 +1,20 @@
 from periphery import I2C
+import time
+import board
+from adafruit_bme280 import basic as adafruit_bme280
+print("Successful imports")
+i2c = board.I2C()
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
-i2c1 = I2C("/dev/i2c-3")  # pins 3/5
+#hPa
+SchenectadySeaLevelPressure = 1004
 
-print("Success")
+bme280.sea_level_pressure = SchenectadySeaLevelPressure
 
-while(True):
-    print("waiting")
+
+while True:
+    print("\nTemperature: %0.1f C" % bme280.temperature)
+    print("Humidity: %0.1f %%" % bme280.relative_humidity)
+    print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("Altitude = %0.2f meters" % bme280.altitude)
+    time.sleep(2)
