6bbfe69d193d9f332c0d33d79c970dc94881f39b
Merek Pipes' VM <pipesj@union.edu>
2023-02-22 01:52:25 -0500

    inputs

--- inputs.py
+++ inputs.py
@@ -2,6 +2,9 @@ import time
 from adafruit_bme280 import basic as adafruit_bme280
 import board
 import busio
+import digitalio
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 print("Successful imports")
 i2c = busio.I2C(board.I2C1_SCL, board.I2C1_SDA)
