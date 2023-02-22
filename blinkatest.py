691a50dcdc60ea4df869fbe6f6c4df3b3647b308
Merek Pipes' VM <pipesj@union.edu>
2023-02-22 00:31:02 -0500

    blinkatest

--- blinkatest.py
+++ blinkatest.py
@@ -10,11 +10,11 @@ pin = digitalio.DigitalInOut(board.GPIO13)
 print("Digital IO ok!")
 
 # Try to create an I2C device
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.I2C1_SCL, board.I2C1_SDA)
 print("I2C ok!")
 
 # Try to create an SPI device
-spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
+spi = busio.SPI(board.SPI_CLK, board.SPI_MI, board.SPI_MO)
 print("SPI ok!")
 
 print("done!")
